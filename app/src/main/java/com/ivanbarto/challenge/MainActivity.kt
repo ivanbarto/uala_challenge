eaad0ca008a9f32819c72f7b0f6a5ba2011b47eb
Ivan Bartolelli <ivanbartolelli@Ivans-MacBook-Pro.local>
2025-06-28 13:27:37 -0300

    project init

---
+++ app/src/main/java/com/ivanbarto/challenge/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ivanbarto.challenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ivanbarto.challenge.ui.theme.ChallengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChallengeTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    TODO: add main entry point
+//                }
+            }
+        }
+    }
+}
